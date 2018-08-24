3faa9c9a634679fae616a9d0f2feef4f216aa631
liu9293 <mylock.kai@gmail.com>
2018-08-24 14:51:34 +0800

    fix async await

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,8 @@ export default [
     },
     plugins: [
       babel({
-        exclude: 'node_modules/**'
+        exclude: 'node_modules/**',
+        runtimeHelpers: true
       }),
       resolve(),
       commonjs(),
